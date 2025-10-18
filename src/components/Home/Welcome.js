3cf28ab0672b69b027d24ebac3da73e6f825e7d7
Mohamed Ramadan <mramadan32@ymail.com>
2025-10-18 11:26:00 +0300

    First commit

--- src/components/Home/Welcome.js
+++ src/components/Home/Welcome.js
@@ -6,6 +6,7 @@ const Welcome = () => {
   return (
     <section className={classes["welcome"]}>
       <h1 className={classes["title"]}>MO Blog</h1>
+
       <p>Hello this blog will teach you web programming from zero</p>
     </section>
   );
